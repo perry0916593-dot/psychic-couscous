425a864f520ac457e826831da086fda77526b01d
perry0916593-dot <perry0916593@gmail.com>
2025-12-04 23:09:56 +0100

    Update signup.js
    Signed-off-by: perry0916593-dot <perry0916593@gmail.com>

--- signup.js
+++ signup.js
@@ -3,16 +3,16 @@ import { auth, createUserWithEmailAndPassword } from "./firebase.js";
 const form = document.getElementById("signupForm");
 
 form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Signup successful!");
-    window.location.href = "login.html";
-  } catch (error) {
-    alert("Error: " + error.message);
-  }
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+        alert("Signup successful!");
+        window.location.href = "login.html";
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
 });
