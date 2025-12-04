7a421583f2a0b1b41a3056e6b19857b983023396
perry0916593-dot <perry0916593@gmail.com>
2025-12-04 21:31:50 +0100

    Update firebase.js
    Signed-off-by: perry0916593-dot <perry0916593@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8m3pqkOg4sATsIM8gkWd-eJC0Z1xRGx0",
-  authDomain: "my-social-app-c1fb8.firebaseapp.com",
-  projectId: "my-social-app-c1fb8",
-  storageBucket: "my-social-app-c1fb8.appspot.com",
+  apiKey: "AIzaSyC8m3pqkOg4sATsI...",
+  authDomain: "my-social-app-c1fbf.firebaseapp.com",
+  projectId: "my-social-app-c1fbf",
+  storageBucket: "my-social-app-c1fbf.appspot.com",
   messagingSenderId: "618305477782",
-  appId: "1:618305477782:web:94ebd181901034e8cfb775"
+  appId: "1:618305477782:web:94e..."
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth
+// Export Firebase Auth
 export const auth = getAuth(app);
-export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
