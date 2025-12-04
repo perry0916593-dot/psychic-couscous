03842f83f3636b0166ff3f672809c04c318bedd3
perry0916593-dot <perry0916593@gmail.com>
2025-12-04 18:26:52 +0100

    Update login.js
    Signed-off-by: perry0916593-dot <perry0916593@gmail.com>

--- login.js
+++ login.js
@@ -1,28 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } 
+    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA7IfFz1-VdHYVEBzNv3toWV2Js8oPMS1Q",
-    authDomain: "c1fb8.firebaseapp.com",
-    projectId: "c1fb8",
-    storageBucket: "c1fb8.appspot.com",
-    messagingSenderId: "324868107508",
-    appId: "1:324868107508:web:3c2ebe37c700bd9a4b4e44"
+  apiKey: "AIzaSyCSxZpKohNzA6YvKoGIGBn6V2mbxCb6jJs",
+  authDomain: "mywebsite-36f07.firebaseapp.com",
+  projectId: "mywebsite-36f07",
+  storageBucket: "mywebsite-36f07.firebasestorage.app",
+  messagingSenderId: "495609232488",
+  appId: "1:495609232488:web:684ca2efa675d2c28ae9dc"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-document.getElementById("loginBtn").addEventListener("click", () => {
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then(() => {
             alert("Login successful!");
-            window.location.href = "home.html"; // you can change this to dashboard.html later
+            window.location.href = "home.html";  
         })
-        .catch(error => {
+        .catch((error) => {
             alert(error.message);
         });
 });
